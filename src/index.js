f69d9c2e7592334865cd526b43503d40a5f1b90c
Mozaniel Pereira e Silva Corrêa <mozanielpcorrea@gmail.com>
2021-07-13 08:11:20 -0300

    Desafio02

--- src/index.js
+++ src/index.js
@@ -10,19 +10,63 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const userVerify = users.find(user => user.username === username);
+  if (!userVerify) {
+    return response.status(404).json({ error: "Error user could not be found" });
+  }
+  request.user = userVerify;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  if (user.pro) {
+    return next();
+  }
+    const todos = user.todos;
+    if (todos.length > 8) {
+      return response.status(403).json({ error: "User is not pro and already have 10 todos" });
+    } 
+    return next();
+  
+
+  
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+  if (!validate(id)) {
+    return response.status(400).json({error:"Invalid id"});
+  }
+
+  const verifyUser = users.find(user => user.username === username);
+  if (!verifyUser) {
+    return response.status(404).json({error:"Invalid username"});
+  }
+  const todoValidate = verifyUser.todos.find(todo => todo.id === id);
+  if (!todoValidate) {
+    return response.status(404).json({error:"invalid id"});
+  }else{
+    request.user = verifyUser;
+    request.todo = todoValidate;
+    return next();
+  }
+  
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params;
+  userValidate = users.find(user => user.id === id);
+  if(!userValidate){
+    return response.status(404).json();
+  }
+
+  request.user = userValidate;
+  return next();
+
 }
 
 app.post('/users', (request, response) => {
